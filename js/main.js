b54d8e664ae5d907ff5a43e584e92dd8eaa1a687
Spike <maxyxxx@mail.ru>
2020-08-11 15:36:10 +0500

    create CreateBoard generate

--- js/main.js
+++ js/main.js
@@ -6,20 +6,36 @@ const createDiv = (parent, classElem)=>{
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    let main = createDiv(document.querySelector('body'), 'main');
-    let game = createDiv(main, 'game');
-    let header = createDiv(game, 'header');
-    header.innerHTML = 'Score: ';
-    let field = createDiv(game, 'field');
-    
     let arrCell = [];
-    for(let i = 0; i < 16; i++){
+    let width = 4;
+    
+    const createBoard = ()=>{
+        let main = createDiv(document.querySelector('body'), 'main');
+        let game = createDiv(main, 'game');
+        let header = createDiv(game, 'header');
+        header.innerHTML = 'Score: ';
+        let field = createDiv(game, 'field');
+        
+        for(let i = 0; i < width * width; i++){
         let cell = createDiv(field, 'cell');
         arrCell.push(cell);
+        }
+        generate()
+        generate()
+    }
+    createBoard();
+
+    //generate a number randomly
+    function generate(){
+        let randomNumber = Math.floor(Math.random() * Math.floor(arrCell.length));
+        if(arrCell[randomNumber].innerHTML == ''){
+            arrCell[randomNumber].innerHTML = 2;
+        }else{generate()}
     }
     
-
     
+
+
 
 
 
